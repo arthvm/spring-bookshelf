ebe221f7482534106ce31908a24841b602fb12b2
arthvm <90999445+arthvm@users.noreply.github.com>
2024-07-31 20:33:36 -0300

    feat: add styled input component

---
+++ root/frontend/src/components/input.tsx
@@ -0,0 +1,42 @@
+import { ComponentProps, ReactNode, useRef } from 'react'
+
+interface InputProp extends ComponentProps<'input'> {
+  icon?: ReactNode
+}
+
+export function Input({ icon, ...props }: InputProp) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      inputRef.current?.focus()
+    }
+  }
+
+  return (
+    <div className="py-2 text-lg text-slate-50 flex items-center justify-start gap-2 md:w-96">
+      <div
+        onClick={handleClick}
+        onKeyDown={handleKeyDown}
+        role="button"
+        tabIndex={0}
+        className="cursor-pointer"
+        aria-label="Focus on input"
+      >
+        {icon}
+      </div>
+      <input
+        ref={inputRef}
+        className="w-[100%] bg-transparent outline-none"
+        {...props}
+      />
+    </div>
+  )
+}
