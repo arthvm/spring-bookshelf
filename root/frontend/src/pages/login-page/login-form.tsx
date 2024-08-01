f5786e81999c16b2f9244091e0bef59517a3754f
arthvm <90999445+arthvm@users.noreply.github.com>
2024-07-31 23:02:10 -0300

    feat: login user when form is submitted

--- root/frontend/src/pages/login-page/login-form.tsx
+++ root/frontend/src/pages/login-page/login-form.tsx
@@ -1,21 +1,52 @@
 import { Button } from '@/components/button'
 import { Input } from '@/components/input'
-import { ArrowRight, Lock, LogIn } from 'lucide-react'
+import { api } from '@/lib/axios'
+import { ArrowRight, Lock, Mail } from 'lucide-react'
+import { FormEvent } from 'react'
 
 export function LoginForm() {
+  async function loginUser(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+
+    const email = String(data.get('email'))
+    const password = String(data.get('password'))
+
+    if (!email || !password) return
+
+    try {
+      const loginResponse = await api.post('/login', {
+        login: email,
+        password,
+      })
+
+      console.log(loginResponse.data.token) // SAVE TOKEN IN THE FUTURE
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <form className="w-[100%] bg-slate-800 shadow-shape rounded-xl p-4 space-y-3 md:w-fit">
+    <form
+      onSubmit={loginUser}
+      className="w-[100%] bg-slate-800 shadow-shape rounded-xl p-4 space-y-3 md:w-fit"
+    >
       <div className="space-y-2">
         <Input
-          icon={<LogIn className="size-5 text-slate-400" />}
+          icon={<Mail className="size-5 text-slate-400" />}
           placeholder="Username or Email"
-          type="text"
+          type="email"
+          name="email"
+          required
         />
 
         <Input
           icon={<Lock className="size-5 text-slate-400" />}
           placeholder="Password"
           type="password"
+          name="password"
+          required
         />
       </div>
 
